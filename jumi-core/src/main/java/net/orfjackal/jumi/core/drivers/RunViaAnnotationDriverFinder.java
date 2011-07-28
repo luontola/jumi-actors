40e5f7b3bc090c3cbd7abbe7c6421080a366988b
Esko Luontola <esko.luontola@gmail.com>
2011-07-28 20:32:16 +0300

    DriverFindersTest

--- jumi-core/src/main/java/net/orfjackal/jumi/core/drivers/RunViaAnnotationDriverFinder.java
+++ jumi-core/src/main/java/net/orfjackal/jumi/core/drivers/RunViaAnnotationDriverFinder.java
@@ -8,7 +8,6 @@ import net.orfjackal.jumi.api.RunVia;
 import net.orfjackal.jumi.api.drivers.Driver;
 
 public class RunViaAnnotationDriverFinder implements DriverFinder {
-    // TODO: cover with unit tests
 
     public Class<? extends Driver> findTestClassDriver(Class<?> testClass) {
         RunVia runVia = testClass.getAnnotation(RunVia.class);
---
+++ jumi-core/src/test/java/net/orfjackal/jumi/core/drivers/DriverFindersTest.java
@@ -0,0 +1,36 @@
+// Copyright © 2011, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package net.orfjackal.jumi.core.drivers;
+
+import net.orfjackal.jumi.api.RunVia;
+import net.orfjackal.jumi.api.drivers.*;
+import org.junit.Test;
+
+import java.util.concurrent.Executor;
+
+import static org.junit.Assert.assertEquals;
+
+public class DriverFindersTest {
+
+    @Test
+    public void finds_driver_from_RunVia_annotation() {
+        assertEquals(DummyDriver.class, findDriverFor(RunViaAnnotatedClass.class));
+    }
+
+    private Class<? extends Driver> findDriverFor(Class<RunViaAnnotatedClass> viaAnnotatedClassClass) {
+        // TODO: support more driver finders: @RunWith, JUnit 4 (@Test), JUnit 3 (TestCase)
+        DriverFinder finder = new RunViaAnnotationDriverFinder();
+        return finder.findTestClassDriver(viaAnnotatedClassClass);
+    }
+
+    @RunVia(DummyDriver.class)
+    private class RunViaAnnotatedClass {
+    }
+
+    private class DummyDriver implements Driver {
+        public void findTests(Class<?> testClass, SuiteNotifier notifier, Executor executor) {
+        }
+    }
+}
