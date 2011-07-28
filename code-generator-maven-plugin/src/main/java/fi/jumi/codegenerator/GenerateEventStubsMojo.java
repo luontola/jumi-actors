94b3db5c40b8d8d7cd700b6ffe775e0ba4d9031f
Esko Luontola <esko.luontola@gmail.com>
2011-07-28 22:34:13 +0300

    Parameters for code generator

---
+++ code-generator-maven-plugin/src/main/java/fi/jumi/codegenerator/GenerateEventStubsMojo.java
@@ -0,0 +1,46 @@
+// Copyright © 2011, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package fi.jumi.codegenerator;
+
+import org.apache.maven.plugin.*;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @goal generate-event-stubs
+ * @phase generate-sources
+ */
+public class GenerateEventStubsMojo extends AbstractMojo {
+
+    /**
+     * @parameter default-value="${project.build.sourceDirectory}"
+     */
+    public File sourceDirectory;
+
+    /**
+     * @parameter default-value="${project.build.directory}/generated-sources/jumi"
+     */
+    public File outputDirectory;
+
+    /**
+     * @parameter
+     */
+    public String targetPackage;
+
+    /**
+     * @parameter
+     */
+    public List<String> eventInterfaces;
+
+    public void execute() throws MojoExecutionException {
+        getLog().info("Hello, world.");
+
+        System.out.println("sourceDirectory = " + sourceDirectory);
+        System.out.println("outputDirectory = " + outputDirectory);
+        System.out.println("targetPackage = " + targetPackage);
+        System.out.println("eventInterfaces = " + eventInterfaces);
+    }
+}
