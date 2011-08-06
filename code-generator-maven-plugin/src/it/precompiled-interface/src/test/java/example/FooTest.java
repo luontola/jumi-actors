1b0b89f54e3f2b5aac6a47a4112c95a35cbe8a13
Esko Luontola <esko.luontola@gmail.com>
2011-08-06 22:44:19 +0300

    Configured maven-invoker-plugin

---
+++ code-generator-maven-plugin/src/it/precompiled-interface/src/test/java/example/FooTest.java
@@ -0,0 +1,17 @@
+// Copyright © 2011, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FooTest {
+
+    @Test
+    public void foo() {
+        assertTrue(true);
+    }
+}
