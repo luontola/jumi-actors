ef22732c48c576c476d179fea648af81d6b326c2
Esko Luontola <esko.luontola@gmail.com>
2011-11-13 20:42:01 +0200

    Refactored SpyListenerTest to use a guinea pig instead of domain class

--- jumi-core/src/test/java/fi/jumi/core/runners/SpyListenerTest.java
+++ jumi-core/src/test/java/fi/jumi/core/runners/SpyListenerTest.java
@@ -4,7 +4,6 @@
 
 package fi.jumi.core.runners;
 
-import fi.jumi.api.drivers.TestId;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,8 +11,8 @@ import static org.hamcrest.Matchers.*;
 
 public class SpyListenerTest {
 
-    private SpyListener<TestClassRunnerListener> spy = new SpyListener<TestClassRunnerListener>(TestClassRunnerListener.class);
-    private TestClassRunnerListener listener = spy.getListener();
+    private SpyListener<DummyListener> spy = new SpyListener<DummyListener>(DummyListener.class);
+    private DummyListener listener = spy.getListener();
 
     @Test
     public void no_expectations() {
@@ -22,16 +21,14 @@ public class SpyListenerTest {
         passes();
     }
 
-    // TODO: use a guinea pig interface instead of TestClassRunnerListener
-
     @Test
     public void matching_expectations() {
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
         spy.replay();
 
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
 
         passes();
     }
@@ -39,115 +36,115 @@ public class SpyListenerTest {
     // TODO: write as white-box unit tests, instead of two "POS" and "NEG" test methods
     @Test
     public void throwable_arguments_are_matched_by_type_and_message_POS() {
-        listener.onFailure(TestId.ROOT, new IllegalArgumentException("foo"));
+        listener.onException(new IllegalArgumentException("foo"));
         spy.replay();
 
-        listener.onFailure(TestId.ROOT, new IllegalArgumentException("foo"));
+        listener.onException(new IllegalArgumentException("foo"));
 
         passes();
     }
 
     @Test
     public void throwable_arguments_are_matched_by_type_and_message_NEG() {
-        listener.onFailure(TestId.ROOT, new IllegalArgumentException("foo"));
+        listener.onException(new IllegalArgumentException("foo"));
         spy.replay();
 
-        listener.onFailure(TestId.ROOT, new IllegalArgumentException("bar"));
+        listener.onException(new IllegalArgumentException("bar"));
 
         fails();
     }
 
     @Test
     public void fails_if_wrong_method_is_called() {
-        listener.onTestStarted(TestId.ROOT);
+        listener.onFirst();
         spy.replay();
 
-        listener.onTestFinished(TestId.ROOT);
+        listener.onSecond();
 
         fails();
     }
 
     @Test
     public void fails_if_right_method_is_called_with_wrong_parameters() {
-        listener.onTestStarted(TestId.of(1));
+        listener.onParameter(1);
         spy.replay();
 
-        listener.onTestFinished(TestId.of(2));
+        listener.onParameter(2);
 
         fails();
     }
 
     @Test
     public void fails_if_right_calls_were_made_in_wrong_order() {
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
         spy.replay();
 
-        listener.onTestFinished(TestId.ROOT);
-        listener.onTestStarted(TestId.ROOT);
+        listener.onSecond();
+        listener.onFirst();
 
         fails();
     }
 
     @Test
     public void fails_if_there_were_fewer_calls_than_expected() {
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
         spy.replay();
 
-        listener.onTestStarted(TestId.ROOT);
+        listener.onFirst();
 
         fails();
     }
 
     @Test
     public void fails_if_there_were_more_calls_than_expected() {
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
         spy.replay();
 
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
+        listener.onSecond();
 
         fails();
     }
 
     @Test
     public void failure_messages_contain_an_ordered_list_of_all_expected_method_calls() {
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
         spy.replay();
 
         String message = fails();
-        assertThat(message, containsString("1. onTestStarted(TestId())"));
-        assertThat(message, containsString("2. onTestFinished(TestId())"));
+        assertThat(message, containsString("1. onFirst()"));
+        assertThat(message, containsString("2. onSecond()"));
     }
 
     @Test
     public void failure_messages_contain_an_ordered_list_of_all_actual_method_calls() {
         spy.replay();
 
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onSecond();
 
         String message = fails();
-        assertThat(message, containsString("1. onTestStarted(TestId())"));
-        assertThat(message, containsString("2. onTestFinished(TestId())"));
+        assertThat(message, containsString("1. onFirst()"));
+        assertThat(message, containsString("2. onSecond()"));
     }
 
     @Test
     public void failure_messages_highlight_the_problematic_calls() {
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.ROOT);
+        listener.onFirst();
+        listener.onParameter(1);
         spy.replay();
 
-        listener.onTestStarted(TestId.ROOT);
-        listener.onTestFinished(TestId.of(1));
+        listener.onFirst();
+        listener.onParameter(2);
 
         String message = fails();
-        assertThat(message, containsString("2. onTestFinished(TestId())\n" + SpyListener.ERROR_MARKER));
-        assertThat(message, containsString("2. onTestFinished(TestId(1))\n" + SpyListener.ERROR_MARKER));
+        assertThat(message, containsString("2. onParameter(1)\n" + SpyListener.ERROR_MARKER));
+        assertThat(message, containsString("2. onParameter(2)\n" + SpyListener.ERROR_MARKER));
         assertThat(occurrencesOf(SpyListener.ERROR_MARKER, message), is(2));
     }
 
@@ -184,5 +181,17 @@ public class SpyListenerTest {
             pos = pos + needle.length();
         }
         return count;
+    }
+
+
+    private interface DummyListener {
+
+        void onFirst();
+
+        void onSecond();
+
+        void onParameter(int parameter);
+
+        void onException(Throwable cause);
     }
 }
