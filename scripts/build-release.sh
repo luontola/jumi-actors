30cb10e98bc6912f33d1c90734f36440c94f62cf
Esko Luontola <esko.luontola@gmail.com>
2012-07-01 00:19:14 +0300

    Take the current working directory from an environment variable
    Avoids "+ pwd" from being printed due to "set -x"

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -3,6 +3,7 @@ set -e
 : ${GO_PIPELINE_COUNTER:?}
 : ${GPG_KEYNAME:?}
 : ${GPG_PASSPHRASE:?}
+: ${PWD:?}
 set -x
 
 ruby scripts/update-to-release-version.rb
@@ -13,4 +14,4 @@ mvn clean deploy \
     -Psonatype-oss-release \
     -Dgpg.keyname="$GPG_KEYNAME" \
     -Dgpg.passphrase="$GPG_PASSPHRASE" \
-    -DaltDeploymentRepository=staging::default::file://`pwd`/staging
+    -DaltDeploymentRepository="staging::default::file://$PWD/staging"
