81350dda84fd78eb2d1d1390c28d711f396d4a52
Esko Luontola <esko.luontola@gmail.com>
2012-07-06 15:58:09 +0300

    Copy staged artifacts from file system
    - Simplifies the code
    - Decouples from Go

--- scripts/promote-staging.sh
+++ scripts/promote-staging.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -e
-: ${GO_PIPELINE_LABEL:?}
-: ${GO_DEPENDENCY_LOCATOR_JUMI:?}
-: ${STAGING_USERNAME:?}
-: ${STAGING_PASSWORD:?}
-: ${RELEASE_USERNAME:?}
-: ${RELEASE_PASSWORD:?}
+: ${DEPLOY_USERNAME:?}
+: ${DEPLOY_PASSWORD:?}
 set -x
 
-ruby scripts/copy-staging-to-release-repo.rb
+VERSION=`cat version`
+
+ruby scripts/upload-maven-repository.rb staging https://oss.sonatype.org/service/local/staging/deploy/maven2
 
 mvn org.sonatype.plugins:nexus-maven-plugin:2.0.6:staging-close \
     --batch-mode \
     --errors \
     -Dnexus.url=https://oss.sonatype.org/ \
-    -Dnexus.username="$RELEASE_USERNAME" \
-    -Dnexus.password="$RELEASE_PASSWORD" \
-    -Dnexus.description="Jumi build $GO_PIPELINE_LABEL"
+    -Dnexus.username="$DEPLOY_USERNAME" \
+    -Dnexus.password="$DEPLOY_PASSWORD" \
+    -Dnexus.description="Jumi $VERSION"
