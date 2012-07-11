ae7220f18b3d176fd282d87c9ad6331e4901375f
Esko Luontola <esko.luontola@gmail.com>
2012-07-12 01:05:09 +0300

    Create tag and release notes on initial build

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -8,6 +8,7 @@ set -x
 
 RELEASE_VERSION=`ruby scripts/get-release-version.rb $GO_PIPELINE_COUNTER`
 CHANGELOG=`ruby scripts/get-release-changelog.rb CHANGELOG.md`
+TAG="jumi-$VERSION"
 
 mkdir build
 echo "$RELEASE_VERSION" > build/version
@@ -28,3 +29,13 @@ mvn clean deploy \
     -Dgpg.keyname="$GPG_KEYNAME" \
     -Dgpg.passphrase="" \
     -DaltDeploymentRepository="staging::default::file://$PWD/staging"
+
+git init --bare staging.git
+
+git tag -u "$GPG_KEYNAME" -m "Jumi $RELEASE_VERSION" -m "$CHANGELOG" "$TAG" "$GO_REVISION_SOURCES"
+git push staging.git "$TAG"
+
+ruby scripts/bump-release-changelog.rb CHANGELOG.md "$RELEASE_VERSION"
+git add CHANGELOG.md
+git commit -m "Updated changelog for Jumi $VERSION release"
+git push staging.git HEAD:master
