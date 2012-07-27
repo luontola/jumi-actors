ff51720444cc23d7bfc102ba07a6211cbe9b2f7a
Esko Luontola <esko.luontola@gmail.com>
2012-07-27 23:49:33 +0300

    Failing end-to-end test for releasing network sockets

--- end-to-end-tests/src/test/java/fi/jumi/test/ReleasingResourcesTest.java
+++ end-to-end-tests/src/test/java/fi/jumi/test/ReleasingResourcesTest.java
@@ -6,12 +6,16 @@ package fi.jumi.test;
 
 import fi.jumi.launcher.JumiLauncher;
 import fi.jumi.test.util.Threads;
+import org.hamcrest.Matcher;
 import org.junit.*;
 
-import java.util.List;
+import java.lang.reflect.*;
+import java.net.*;
+import java.util.*;
 
 import static fi.jumi.test.util.CollectionMatchers.containsAtMost;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class ReleasingResourcesTest {
 
@@ -32,12 +36,100 @@ public class ReleasingResourcesTest {
         assertThat(threadsAfter, containsAtMost(threadsBefore));
     }
 
-    // TODO: network sockets
+    @Ignore("not implemented")
+    @Test(timeout = TIMEOUT)
+    public void launcher_closes_all_server_sockets_it_opened() throws Exception {
+        List<SocketImpl> allSockets = Collections.synchronizedList(new ArrayList<SocketImpl>());
+        ServerSocket.setSocketFactory(new SpySocketImplFactory(allSockets));
 
+        startAndStopLauncher();
+
+        for (SocketImpl impl : allSockets) {
+            assertIsClosed(getServerSocket(impl));
+        }
+    }
+
+    @Ignore("not implemented")
+    @Test(timeout = TIMEOUT)
+    public void launcher_closes_all_client_sockets_it_opened() throws Exception {
+        List<SocketImpl> allSockets = Collections.synchronizedList(new ArrayList<SocketImpl>());
+        Socket.setSocketImplFactory(new SpySocketImplFactory(allSockets));
+
+        startAndStopLauncher();
+
+        for (SocketImpl impl : allSockets) {
+            assertIsClosed(getSocket(impl));
+        }
+    }
 
     private void startAndStopLauncher() throws Exception {
         app.runTests("unimportant");
         JumiLauncher launcher = app.getLauncher();
         launcher.close();
+    }
+
+
+    // asserts
+
+    private static void assertIsClosed(Socket socket) {
+        assertThat(socket.isClosed(), isClosed(socket));
+    }
+
+    private static void assertIsClosed(ServerSocket socket) {
+        assertThat(socket.isClosed(), isClosed(socket));
+    }
+
+    private static Matcher<Boolean> isClosed(Object socket) {
+        return describedAs("is closed: %0", is(true), socket);
+    }
+
+
+    // socket helpers
+
+    private static SocketImpl newSocketImpl() {
+        try {
+            Class<?> defaultSocketImpl = Class.forName("java.net.SocksSocketImpl");
+            Constructor<?> constructor = defaultSocketImpl.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return (SocketImpl) constructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Socket getSocket(SocketImpl impl) {
+        try {
+            Method getSocket = SocketImpl.class.getDeclaredMethod("getSocket");
+            getSocket.setAccessible(true);
+            return (Socket) getSocket.invoke(impl);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static ServerSocket getServerSocket(SocketImpl impl) {
+        try {
+            Method getServerSocket = SocketImpl.class.getDeclaredMethod("getServerSocket");
+            getServerSocket.setAccessible(true);
+            return (ServerSocket) getServerSocket.invoke(impl);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class SpySocketImplFactory implements SocketImplFactory {
+
+        private final List<SocketImpl> spy;
+
+        public SpySocketImplFactory(List<SocketImpl> spy) {
+            this.spy = spy;
+        }
+
+        @Override
+        public SocketImpl createSocketImpl() {
+            SocketImpl socket = newSocketImpl();
+            spy.add(socket);
+            return socket;
+        }
     }
 }
