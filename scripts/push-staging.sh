8401c1ae319996d7d9af7f5132c9c15d91f7a38d
Esko Luontola <esko.luontola@gmail.com>
2012-08-06 02:03:20 +0300

    Extracted checking the release notes into its own stage

--- scripts/push-staging.sh
+++ scripts/push-staging.sh
@@ -3,15 +3,6 @@ set -eu
 : ${GIT_REPOSITORY:?}
 set -x
 
-RELEASE_NOTES=`cat build/release-notes`
-
-# Require the release notes to contain something else than just the TBD placeholder
-if echo "$RELEASE_NOTES" | grep --line-regexp --quiet "\- TBD"
-then
-    echo "Release notes not filled in"
-    exit 1
-fi
-
 git clone "$GIT_REPOSITORY" work
 cd work
 git remote add staging ../staging.git
