3041b8e37248b558a2d34bda3d58e68516ff79bc
Esko Luontola <esko.luontola@gmail.com>
2012-09-13 21:33:11 +0300

    Refactored

--- jumi-core/src/test/java/fi/jumi/core/output/OutputCapturerTest.java
+++ jumi-core/src/test/java/fi/jumi/core/output/OutputCapturerTest.java
@@ -14,7 +14,6 @@ import java.util.concurrent.*;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.*;
 
 public class OutputCapturerTest {
 
@@ -32,46 +31,53 @@ public class OutputCapturerTest {
     @Test
     public void passes_through_stdout_to_the_real_stdout() {
         capturer.out().print("foo");
-        capturer.out().write('.');
 
-        assertThat(printedToOut.toString(), is("foo."));
+        assertThat(printedToOut.toString(), is("foo"));
     }
 
     @Test
     public void captures_stdout() {
-        OutputListener listener = mock(OutputListener.class);
+        OutputListenerSpy listener = new OutputListenerSpy();
 
         capturer.captureTo(listener);
         capturer.out().print("foo");
+
+        assertThat(listener.out).containsExactly("foo");
+    }
+
+    @Test
+    public void single_byte_prints_are_also_captured_and_passed_through() {
+        OutputListenerSpy listener = new OutputListenerSpy();
+        capturer.captureTo(listener);
+
         capturer.out().write('.');
 
-        verify(listener).out("foo");
-        verify(listener).out(".");
-        verifyNoMoreInteractions(listener);
+        assertThat(printedToOut.toString(), is("."));
+        assertThat(listener.out).containsExactly(".");
     }
 
     @Test
     public void after_starting_a_new_capture_all_new_events_to_to_the_new_output_listener() {
-        OutputListener listener1 = mock(OutputListener.class, "listener1");
-        OutputListener listener2 = mock(OutputListener.class, "listener2");
+        OutputListenerSpy listener1 = new OutputListenerSpy();
+        OutputListenerSpy listener2 = new OutputListenerSpy();
 
         capturer.captureTo(listener1);
         capturer.captureTo(listener2);
         capturer.out().print("foo");
 
-        verifyZeroInteractions(listener1);
-        verify(listener2).out("foo");
+        assertThat(listener1.out).containsExactly();
+        assertThat(listener2.out).containsExactly("foo");
     }
 
     @Test
     public void starting_a_new_capture_does_not_require_installing_a_new_PrintStream_to_SystemOut() {
-        OutputListener listener = mock(OutputListener.class);
+        OutputListenerSpy listener = new OutputListenerSpy();
 
         PrintStream out = capturer.out();
         capturer.captureTo(listener);
         out.print("foo");
 
-        verify(listener).out("foo");
+        assertThat(listener.out).containsExactly("foo");
     }
 
 
