5416840bca6e068a4a22feb2f525c19bf23aee81
Esko Luontola <esko.luontola@gmail.com>
2014-11-13 21:52:52 +0200

    Upgraded to BuildTest 1.0.2

--- end-to-end-tests/src/test/java/fi/jumi/test/TestEnvironment.java
+++ end-to-end-tests/src/test/java/fi/jumi/test/TestEnvironment.java
@@ -1,4 +1,4 @@
-// Copyright © 2011-2013, Esko Luontola <www.orfjackal.net>
+// Copyright © 2011-2014, Esko Luontola <www.orfjackal.net>
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
@@ -16,7 +16,7 @@ public class TestEnvironment {
 
     static {
         Properties p = ResourcesUtil.getProperties("testing.properties");
-        ARTIFACTS = new ProjectArtifacts(getDirectory(p, "test.projectArtifactsDir"));
+        ARTIFACTS = new ProjectArtifacts(getDirectory(p, "test.projectArtifactsDir"), VERSION_NUMBERING);
     }
 
     private static File getDirectory(Properties properties, String key) {
