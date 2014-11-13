e7afeb9c080d241b86a325e7f7ba33d779c36a5a
Esko Luontola <esko.luontola@gmail.com>
2014-11-13 20:15:05 +0200

    Code generator walking skeleton

--- jumi-actors-generator/src/main/java/fi/jumi/actors/generator/AnnotationProcessor.java
+++ jumi-actors-generator/src/main/java/fi/jumi/actors/generator/AnnotationProcessor.java
@@ -4,25 +4,52 @@
 
 package fi.jumi.actors.generator;
 
+import com.google.common.base.Throwables;
+
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
+import javax.lang.model.element.*;
+import javax.tools.JavaFileObject;
+import java.io.*;
 import java.util.Set;
+
+import static javax.tools.Diagnostic.Kind.*;
 
 @SupportedAnnotationTypes("fi.jumi.actors.generator.GenerateEventizer")
 public class AnnotationProcessor extends AbstractProcessor {
 
     @Override
-    public synchronized void init(ProcessingEnvironment processingEnv) {
-        super.init(processingEnv);
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "init");
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Messager log = processingEnv.getMessager();
+        for (Element element : roundEnv.getElementsAnnotatedWith(GenerateEventizer.class)) {
+            if (element.getKind() == ElementKind.INTERFACE) {
+                log.printMessage(NOTE, "Generating eventizers for " + element, element);
+                try {
+                    generateEventizers((TypeElement) element);
+                } catch (Exception e) {
+                    log.printMessage(ERROR, "Failed to generate eventizers for " + element +
+                            "\n" + Throwables.getStackTraceAsString(e), element);
+                }
+            } else {
+                log.printMessage(WARNING, "Only interfaces can be annotated with @GenerateEventizer", element);
+            }
+        }
+        return false;
     }
 
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.OTHER, "process");
-        return false;
+    private void generateEventizers(TypeElement iface) throws IOException {
+        PackageElement pkg = getPackage(iface);
+        JavaFileObject file = processingEnv.getFiler().createSourceFile(pkg.getQualifiedName() + ".Foo");
+        Writer w = file.openWriter();
+        w.write("package " + pkg.getQualifiedName() + "; class Foo {}");
+        w.close();
+    }
+
+    private static PackageElement getPackage(Element e) {
+        while (e.getKind() != ElementKind.PACKAGE) {
+            e = e.getEnclosingElement();
+        }
+        return (PackageElement) e;
     }
 
     @Override
