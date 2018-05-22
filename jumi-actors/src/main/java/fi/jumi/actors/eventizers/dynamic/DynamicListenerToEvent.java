7268ebfe23ddab58055f12589dbb13ab60217952
Esko Luontola <esko@luontola.fi>
2018-05-23 01:34:28 +0300

    Check for the Future interface instead of the custom Promise class
    Fixes #5

--- jumi-actors/src/main/java/fi/jumi/actors/eventizers/dynamic/DynamicListenerToEvent.java
+++ jumi-actors/src/main/java/fi/jumi/actors/eventizers/dynamic/DynamicListenerToEvent.java
@@ -1,4 +1,4 @@
-// Copyright © 2011-2015, Esko Luontola <www.orfjackal.net>
+// Copyright © 2011-2018, Esko Luontola <www.orfjackal.net>
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
@@ -10,6 +10,7 @@ import fi.jumi.actors.queue.MessageSender;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.lang.reflect.*;
+import java.util.concurrent.Future;
 
 @ThreadSafe
 public class DynamicListenerToEvent<T> implements InvocationHandler {
@@ -25,7 +26,7 @@ public class DynamicListenerToEvent<T> implements InvocationHandler {
         if (method.getDeclaringClass().equals(Object.class)) {
             return method.invoke(this, args);
         }
-        if (method.getReturnType().isAssignableFrom(Promise.class)) {
+        if (Future.class.isAssignableFrom(method.getReturnType())) {
             Promise.Deferred<T> deferred = Promise.defer();
             target.send(new DynamicEvent<>(method, args, deferred));
             return deferred.promise();
